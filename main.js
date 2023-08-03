57c5f0488504c9d3172fc7312f3d42160da69cdf
nikhilKumar131 <nikhilkumarofficial131@gmail.com>
2023-08-03 23:28:58 +0530

    changes

--- main.js
+++ main.js
@@ -40,11 +40,11 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark"); //add this
-  } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light"); //add this
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //add this
   }
 }
 
